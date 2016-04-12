10b0c22fbb57f756e491ef638b3c8caa6763b815
Lucas Almeida <Lucas Almeida>
2016-04-12 13:41:08 -0300

    Atualizando Repositório

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>relatorio.nwm</title>
+	<style type="text/css" media="screen">
+		body{
+			background: #000;
+			color: #54ff00;
+			padding-left: 10%;
+			padding-right: 10%;
+		}
+		p.dd{
+			float: left;
+		}
+	</style>
+</head>
+<body>
+<p class="dd"><a href="http://127.0.0.1/ACI/" title=""><< voltar</a></p>
+<center>
+<h1>RELAT&Oacute;RIOS</h1>
+<table border="1" width="100%">
+	<thead>
+		<tr>
+			<th>##ID##</th>
+			<th>Relatório</th>
+			<th>Criação</th>
+			<th>Modificação</th>
+			<th>Ação</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>00</td>
+			<td>AAAAAA</td>
+			<td>00/00/0000</td>
+			<td>00/00/0000</td>
+			<td><center><a href="#" title="">Visualizar</a></center></td>
+		</tr>
+	</tbody>
+</table>
+</center>
+
+</body>
+</html>
